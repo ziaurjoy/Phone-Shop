e7c36a03deb43b0ac202fe5581b0416ab87624b7
ZiaurJoy <ziaurjoy802@gmail.com>
2021-03-20 15:00:17 +0600

    React build and Django statis fiels setting

--- phone_shop/urls.py
+++ phone_shop/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    # path('api/', include('backend.urls')),
+    re_path(r'^.*', TemplateView.as_view(template_name='index.html')),
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
