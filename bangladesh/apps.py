b2a72184f36af614e145c890bf4899558a8967bb
ZiaurJoy <ziaurjoy802@gmail.com>
2021-03-22 12:41:19 +0600

    remove Bangladesh

--- bangladesh/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class BangladeshConfig(AppConfig):
-    name = 'bangladesh'
--- bangladesh/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http.response import HttpResponse
-# Create your views here.
-def hello(request):
-    return HttpResponse('Feni')
