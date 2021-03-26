84763245c8f7f79c1c0b38ca37be0ad43f52a47d
ZiaurJoy <ziaurjoy802@gmail.com>
2021-03-26 22:17:17 +0600

    Global state create

--- src/state/reducer.js
+++ src/state/reducer.js
@@ -1,23 +1,4 @@
 
-// export const initialstate = {
-//     profile:null,
-// }
-
-// const reducher = (state,action) =>{
-//     switch (action.type){
-//         case "ADD_PROFILE":
-//             return {
-//                 ... state,
-//                 profile: action.profile,
-//             }
-//         default:
-//             return state;
-
-//     }
-// }
-
-// export default reducher
-
 
 
 export const initialstate = {
